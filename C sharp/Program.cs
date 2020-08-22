44895555b25f05efa4eac634d38bbd0a9caafa1e
unknown <lucasscruz2001@gmail.com>
2020-08-22 15:34:36 -0300

    Acrescentado um console teste

--- C sharp/Program.cs
+++ C sharp/Program.cs
@@ -7,6 +7,7 @@ namespace C_sharp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Teste de Console.WriteLine");
         }
     }
 }
